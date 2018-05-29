e23256d199e48f51f442da2bc69058a06d2c9677
wangyuandong <yuandong@geekniu.com>
2018-05-29 14:29:07 +0800

    Initial Commit

---
+++ WYDThoughts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WYDThoughts
+//
+//  Created by 王远东 on 2018/5/29.
+//  Copyright © 2018年 wangyuandong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
